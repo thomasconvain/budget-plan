c673f44b08f562d5dc51006520ebf5fa34f11b46
thomas.convain <thomas.convain@gmail.com>
2024-08-18 18:50:11 -0400

    recaptccha

--- src/firebase.js
+++ src/firebase.js
@@ -1,5 +1,7 @@
+/* eslint-disable no-unused-vars */
 // src/firebase.js
 import { initializeApp } from "firebase/app";
+import { initializeAppCheck, ReCaptchaV3Provider } from "firebase/app-check";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 // import { getAnalytics } from "firebase/analytics";
@@ -18,6 +20,15 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 // const analytics = getAnalytics(app);
+// Pass your reCAPTCHA v3 site key (public key) to activate(). Make sure this
+// key is the counterpart to the secret key you set in the Firebase console.
+const appCheck = initializeAppCheck(app, {
+  provider: new ReCaptchaV3Provider('6Le1rCkqAAAAAOpfur97XLWyl-GZBiTl63Gx48Cy'),
+
+  // Optional argument. If true, the SDK automatically refreshes App Check
+  // tokens as needed.
+  isTokenAutoRefreshEnabled: true
+});
 
 // Inicializa los servicios que necesites
 const auth = getAuth(app);
