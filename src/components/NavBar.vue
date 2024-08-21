c789044063f5eef934ce97e6e38ebdaa559b2da4
thomas.convain <thomas.convain@gmail.com>
2024-08-21 11:24:36 -0400

    update de foto perfil por defecto

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -34,7 +34,8 @@
               <MenuButton class="relative flex rounded-full border border-gray-500 bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
                 <span class="absolute -inset-1.5" />
                 <span class="sr-only">Open user menu</span>
-                <img class="h-10 w-10 rounded-full" :src="user?.photoURL ? user?.photoURL : defaultPhotoUrl" alt="" />
+                <img v-if="user?.photoURL" class="h-10 w-10 rounded-full" :src="user?.photoURL" alt="" />
+                <img class="h-10 w-10 rounded-full" src="../assets/img/default_profile.jpg" alt="" />
               </MenuButton>
             </div>
             <transition enter-active-class="transition ease-out duration-100" enter-from-class="transform opacity-0 scale-95" enter-to-class="transform opacity-100 scale-100" leave-active-class="transition ease-in duration-75" leave-from-class="transform opacity-100 scale-100" leave-to-class="transform opacity-0 scale-95">
@@ -69,8 +70,6 @@ import { Disclosure, DisclosureButton, DisclosurePanel, Menu, MenuButton, MenuIt
 import { Bars3Icon, XMarkIcon } from '@heroicons/vue/24/outline'
 import { useStore } from 'vuex';
 import { computed } from 'vue';
-
-const defaultPhotoUrl = 'https://images.unsplash.com/photo-1641984790242-d8aa477d306c?q=80&w=1935&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
 
 const store = useStore();
 
