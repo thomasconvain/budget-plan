ee3abeb87cf69b190446fbd060fab40e26e99030
Thomas Convain <tconvain@creditu.com>
2025-04-21 20:29:21 -0400

    se habilita archivar tarjetas antiguas para mantenerlas a las vista y no afectar saldo general

--- src/components/ListGoals.vue
+++ src/components/ListGoals.vue
@@ -25,10 +25,22 @@
                   Se factura en {{calculateDaysRemaining(goal.validUntil.toDate()) }}  {{ calculateDaysRemaining(goal.validUntil.toDate()) <= 1 ? 'día' : 'días' }}
                 </p>
                 <p 
-                  v-else
+                  v-else-if="goal.type !== 'Tarjeta de crédito'"
                   class="text-sm text-gray-500">
                   Sin fecha de facturación
                 </p>
+                <span 
+                  v-else
+                  class="text-sm text-gray-500 flex items-center gap-x-2">
+                  <p>
+                    Se ha facturado esta tarjeta
+                  </p>
+                  <button
+                    class="px-4 py-2 text-xs font-medium text-indigo-700 bg-indigo-50 border border-transparent rounded-lg hover:bg-indigo-100 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                    @click.stop.prevent="archiveGoal(goal.id)">
+                      Archivar
+                  </button>
+                </span>
               </div>
               <div class="text-right ml-auto">
                 <p class="text-md text-gray-800 font-semibold flex items-center gap-x-2">
@@ -49,6 +61,28 @@
       </ul>
     </div>
     <p v-else class="my-8 text-gray-400">Aún no tienes tarjeta agregada</p>
+    <div v-if="archivedCreditCardGoals.length > 0" class="max-w-4xl mx-auto">
+      <p class="my-2 text-gray-400">Tu tarjetas archivadas:</p>
+      <ul role="list" class="">
+        <li v-for="goal in archivedCreditCardGoals" :key="goal.id" class="mb-4">
+          <div class="flex items-center justify-between z-2 relative px-4 py-4 bg-white shadow-lg rounded-lg hover:bg-gray-50">
+            <router-link :to="`/goal/${goal.id}`" class="flex items-center flex-1">
+              <CreditCardIcon class="h-6 w-6" aria-hidden="true" />
+              <div class="ml-4">
+                <p class="text-sm font-medium text-gray-900">{{ goal.title }}</p>
+              </div>
+              <div class="text-right ml-auto">
+                <p class="text-md text-gray-800 font-semibold flex items-center gap-x-2">
+                  <ArrowUpIcon class="h-4 w-4 text-red-600" />
+                  ${{ formatNumber(goal.currentBalanceOnAccount, 'CLP') }}
+                </p>
+              </div>
+            </router-link>
+            <button @click="handleDeleteGoal(goal.id)" class="ml-4 text-slate-300 hover:text-red-600"><TrashIcon class="h-4 w-4" aria-hidden="true" /></button>
+          </div>
+        </li>
+      </ul>
+    </div>
     <h1 class="text-2xl font-semibold	mb-4 mt-16">Tus cuentas bancarias</h1>
     <div v-if="bankAccountGoals.length > 0" class="max-w-4xl mx-auto">
       <ul role="list" class="">
@@ -84,7 +118,7 @@
 <script setup>
 import { ref, onMounted, computed } from 'vue';
 import { ArrowUpIcon,ArrowDownIcon, CurrencyDollarIcon, CreditCardIcon, TrashIcon, PlusCircleIcon } from '@heroicons/vue/24/outline';
-import { getFirestore, collection, getDocs, query, where, deleteDoc, doc, writeBatch } from 'firebase/firestore';
+import { getFirestore, collection, getDocs, updateDoc, query, where, deleteDoc, doc, writeBatch } from 'firebase/firestore';
 import { getAuth, onAuthStateChanged } from 'firebase/auth';
 import {fetchGoals} from '../utils/business/goals.js'
 import LoadingSpinner from '../components/LoadingSpinner.vue'
@@ -98,7 +132,8 @@ const isLoading = ref('true')
 const goals = ref([]);
 const goalsTotalBalance = ref(0);
 
-const creditCardGoals = computed(() => goals.value.filter(goal => goal.type === 'Tarjeta de crédito'));
+const creditCardGoals = computed(() => goals.value.filter(goal => goal.type === 'Tarjeta de crédito' && goal.isArchived !== true));
+const archivedCreditCardGoals = computed(() => goals.value.filter(goal => goal.type === 'Tarjeta de crédito' && goal.isArchived === true));
 const bankAccountGoals = computed(() => goals.value.filter(goal => goal.type === 'Cuenta bancaria'));
 
 onMounted(() => {
@@ -108,7 +143,7 @@ onMounted(() => {
       goals.value = await fetchGoals();
       // Sumar los valores para 'Tarjeta de crédito'
       const creditCardTotal = goals.value
-        .filter(goal => goal.type === 'Tarjeta de crédito')
+        .filter(goal => goal.type === 'Tarjeta de crédito' && goal.isArchived === false)
         .reduce((sum, goal) => sum + goal.currentBalanceOnAccount, 0);
 
       // Sumar los valores para 'Cuenta bancaria'
@@ -144,6 +179,19 @@ const handleDeleteGoal = async (goalId) => {
     goals.value = await fetchGoals();
   }
 };
+
+const archiveGoal = async (goalId) => {
+  try {
+    const goalRef = doc(db, 'goals', goalId)
+    await updateDoc(goalRef, { isArchived: true })
+    console.log(`Goal ${goalId} archivado correctamente`)
+    // aquí podrías emitir un evento o actualizar un state local
+    // Actualizar la lista de presupuestos después de eliminar
+    goals.value = await fetchGoals();
+  } catch (error) {
+    console.error('Error archivando goal:', error)
+  }
+}
 
 const calculateDaysRemaining = (targetDate) => {
   const today = new Date();
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,16 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  devServer: {
+    client: {
+      overlay: {
+        // errores de compilación (syntax, type, etc.)
+        errors:   true,
+        // warnings de compilación
+        warnings: false,
+        // errores en tiempo de ejecución (unhandled promise rejections, excepciones uncaught)
+        runtimeErrors: false
+      }
+    }
+  }
 })
