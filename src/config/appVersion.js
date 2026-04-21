abdf671e0a7f8e6db97a62f461a926711705fd94
Thomas Convain <tconvain@creditu.com>
2026-04-21 13:19:42 -0400

    Actualiza la versión de la aplicación a 3.2.5 en build.gradle y package.json. Sincroniza los valores de versión en appVersion.js.

--- src/config/appVersion.js
+++ src/config/appVersion.js
@@ -1,8 +1,8 @@
 // Mantener sincronizado con android/app/build.gradle:
 //   versionCode  -> CURRENT_ANDROID_VERSION_CODE
 //   versionName  -> CURRENT_ANDROID_VERSION_NAME
-export const CURRENT_ANDROID_VERSION_CODE = 32
-export const CURRENT_ANDROID_VERSION_NAME = '3.2.4'
+export const CURRENT_ANDROID_VERSION_CODE = 33
+export const CURRENT_ANDROID_VERSION_NAME = '3.2.5'
 
 export const ANDROID_PACKAGE_NAME = 'com.budgetplanapp'
 export const PLAY_STORE_URL = `https://play.google.com/store/apps/details?id=${ANDROID_PACKAGE_NAME}`
